f40d42600592d7cdb767427328b02a2794fa1db7
ardisaurus <ardinisme@gmail.com>
2021-09-10 23:03:18 +0700

    feat : add status selector

--- src/components/PaymentStatusSelector.js
+++ src/components/PaymentStatusSelector.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 240,
+    },
+}));
 
 export default function PaymentStatusSelector() {
-    return (
-        <div>
+    const classes = useStyles();
+    const [status, setStatus] = useState('accept');
 
-        </div>
+    return (
+        <>
+            <tr>
+                <td>Status Request</td>
+                <td
+                    className={classes.centerCell}>:</td>
+                <td>
+                    <FormControl variant="outlined" className={classes.formControl}>
+                        <Select
+                            value={status}
+                            onChange={(e) => setStatus(e.target.value)}
+                        >
+                            <MenuItem value="accept">Accept</MenuItem>
+                            <MenuItem value="reject">Reject</MenuItem>
+                        </Select>
+                    </FormControl>
+                </td>
+            </tr>
+            {status === "reject" && (<tr>
+                <td>Alasan</td>
+                <td
+                    className={classes.centerCell}>:</td>
+                <td>
+                    <TextField
+                        label="Multiline"
+                        multiline
+                        maxRows={6}
+                        minRows={4}
+                        variant="outlined"
+                        className={classes.formControl}
+                    />
+                </td>
+            </tr>)}
+
+        </>
     )
 }
--- src/pages/officer/accounting/PaymentRequestDetail.js
+++ src/pages/officer/accounting/PaymentRequestDetail.js
@@ -1,9 +1,64 @@
-import React from 'react'
 
-export default function PaymentRequestDetail() {
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import BackIcon from "@material-ui/icons/ChevronLeft";
+import ContentContainer from "../../../components/ContentContainer";
+import PaymentStatusSelector from "../../../components/PaymentStatusSelector";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    root: {
+        width: "100%",
+        paddingBottom: "1em",
+    },
+    paper: {
+        padding: "1em",
+    },
+    tableContainer: {
+        display: "flex", justifyContent: "center"
+    },
+    centerCell: {
+        padding: "0.5em 1em"
+    }
+});
+
+
+function PaymentRequestDetail() {
+    const classes = useStyles();
+
     return (
-        <div>
-
-        </div>
-    )
+        <ContentContainer role="generalSupport">
+            <div
+                className={classes.root}
+            >
+                <Typography variant="h5">Payment Request</Typography>
+            </div>
+            <Paper
+                className={classes.paper}>
+                <div
+                    className={classes.tableContainer}>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Diminta oleh</td>
+                                <td
+                                    className={classes.centerCell}>:</td>
+                                <td>Asep Sunandar</td>
+                            </tr>
+                            <PaymentStatusSelector />
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <Button startIcon={<BackIcon />} variant="contained" color="primary">
+                        Kembali
+                    </Button>
+                </div>
+            </Paper>
+        </ContentContainer>
+    );
 }
+
+export default PaymentRequestDetail;
