54d01e3a664986523e813d48b8eac101938f1f58
khalikabd <khalikabd5@gmail.com>
2021-09-17 12:03:56 +0800

    change : Status Selector

--- src/components/PaymentStatusSelector.js
+++ src/components/PaymentStatusSelector.js
@@ -1,61 +1,89 @@
-import React, { useState } from 'react'
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import { makeStyles } from "@material-ui/core/styles";
+import React, {useState} from "react";
 
-const useStyles = makeStyles((theme) => ({
-    formControl: {
-        margin: theme.spacing(1,0),
-        minWidth: 230,
+// Material ui core
+import {
+    FormControl,
+    MenuItem,
+    Select,
+    TextField,
+    TableRow
+} from "@material-ui/core";
+import MuiTableCell from "@material-ui/core/TableCell";
+import { withStyles} from "@material-ui/core/styles";
+import useStyles from "../styles/HasilFormPayement";
+
+const TableCell = withStyles({
+    root: {
+        borderBottom: "none",
+        fontSize: "18px",
+        fontWeight: 600,
     },
-    centerCell: {
-        padding: "0.8em 3em"
-    },
+})(MuiTableCell);
 
-}));
+const createData = (description, value) => {
+    return { description, value };
+  };
 
-export default function PaymentStatusSelector() {
+const rows = [
+    createData("Diminta Oleh", "Asep Sunandar"),
+    createData("Keperluan Payment", "SPP Juli 2020"),
+    createData("Tanggal Pembayaran", "Sabtu, 10 Juli 2020"),
+    createData("Jumlah Payment", "Rp. 1.000.000"),
+    createData("Terbilang", "Satu juta rupiah"),
+    createData("Nama Rek. / Penerima", "MD. Mubarokul Huda"),
+    createData("No. Rekening Penerima", "15000757050"),
+    createData("Request Terkirim", "Jum’at, 9 Juli 2021 (09.00 PM)"),
+    createData("Status Request", "null"),
+  ];
+
+function PaymentStatusSelector() {
     const classes = useStyles();
     const [status, setStatus] = useState('null');
 
     return (
         <>
-            <tr>
-                <td>Status Request</td>
-                <td
-                    className={classes.centerCell}>:</td>
-                <td>
-                    <FormControl variant="outlined" className={classes.formControl}>
-                        <Select
-                            value={status}
-                            onChange={(e) => setStatus(e.target.value)}
-                        >
-                            <MenuItem value="null">-Ubah Status-</MenuItem>
-                            <MenuItem value="accept">Accept</MenuItem>
-                            <MenuItem value="reject">Reject</MenuItem>
-                        </Select>
-                    </FormControl>
-                </td>
-            </tr>
-            {status === "reject" && (<tr>
-                <td>Alasan</td>
-                <td
-                    className={classes.centerCell}>:</td>
-                <td>
-                    <TextField
-                        // label="Multiline"
-                        multiline
-                        maxRows={6}
-                        minRows={4}
-                        variant="outlined"
-                        className={classes.formControl}
-                    /> 
-                </td>
-            </tr>)}
+           {rows.map((row)=>(
+                <TableRow key={row.name}>
+                    <TableCell>{row.description}</TableCell>
+                    <TableCell>:</TableCell>
+                    {row.value === "null"  ? (
+                        <TableCell>
+                            <FormControl variant="outlined" className={classes.formControl}>
+                            <Select
+                                value={status}
+                                onChange={(e) => setStatus(e.target.value)}
+                                style={{width:350}}
+                            >
+                                <MenuItem value="null">-Ubah Status-</MenuItem>
+                                <MenuItem value="accept">Accept</MenuItem>
+                                <MenuItem value="reject">Reject</MenuItem>
+                            </Select>
+                            </FormControl>
+                        </TableCell>
+                    ) : (
+                        <TableCell>{row.value}</TableCell>
+                    )}                                        
+                </TableRow>
+            ))}
+            {status === "reject" ? (
+                <TableRow>
+                    <TableCell>Alasan</TableCell>
+                    <TableCell>:</TableCell>
+                    <TableCell>
+                            <TextField
+                                multiline
+                                maxRows={6}
+                                minRows={4}
+                                variant="outlined"
+                                style={{width:350}}/>
+                    </TableCell>
+                </TableRow>
+                ) : (
+                    <TableCell> </TableCell>
+                )}
             
         </>
        
     )
 }
+export default PaymentStatusSelector
--- src/pages/officer/accounting/PaymentRequestDetail.js
+++ src/pages/officer/accounting/PaymentRequestDetail.js
@@ -1,6 +1,3 @@
-
-import React, {useState} from "react";
-
 // Material ui core
 import {
     Typography,
@@ -9,53 +6,19 @@ import {
     CardActions,
     CardContent,
     Container,
-    FormControl,
-    MenuItem,
     Table,
     TableBody,
     TableContainer,
-    Select,
-    TextField,
-    TableRow
+  
 } from "@material-ui/core";
-import MuiTableCell from "@material-ui/core/TableCell";
-
-// Material ui icons
 import SaveRoundedIcon from '@material-ui/icons/SaveRounded';
 import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded';
-
-// 
 import ContentContainer from "../../../components/ContentContainer";
-import { withStyles} from "@material-ui/core/styles";
+import PaymentStatusSelector from "../../../components/PaymentStatusSelector";
 import useStyles from "../../../styles/HasilFormPayement";
-
-const TableCell = withStyles({
-    root: {
-        borderBottom: "none",
-        fontSize: "18px",
-        fontWeight: 600,
-    },
-})(MuiTableCell);
-
-const createData = (description, value) => {
-    return { description, value };
-  };
-
-const rows = [
-    createData("Diminta Oleh", "Asep Sunandar"),
-    createData("Keperluan Payment", "SPP Juli 2020"),
-    createData("Tanggal Pembayaran", "Sabtu, 10 Juli 2020"),
-    createData("Jumlah Payment", "Rp. 1.000.000"),
-    createData("Terbilang", "Satu juta rupiah"),
-    createData("Nama Rek. / Penerima", "MD. Mubarokul Huda"),
-    createData("No. Rekening Penerima", "15000757050"),
-    createData("Request Terkirim", "Jum’at, 9 Juli 2021 (09.00 PM)"),
-    createData("Status Request", "null"),
-  ];
 
 function PaymentRequestDetail(props) {
     const classes = useStyles();
-    const [status, setStatus] = useState('null');
     const handleClickGoBack = () => {
         props.history.goBack();
       }
@@ -77,71 +40,32 @@ function PaymentRequestDetail(props) {
                             <TableContainer className={classes.table}>
                                 <Table className={classes.table} aria-label="simple table">
                                     <TableBody>
-                                        {rows.map((row)=>(
-                                            <TableRow key={row.name}>
-                                                <TableCell>{row.description}</TableCell>
-                                                <TableCell>:</TableCell>
-                                                {row.value === "null"  ? (
-                                                    <TableCell>
-                                                        <FormControl variant="outlined" className={classes.formControl}>
-                                                        <Select
-                                                            value={status}
-                                                            onChange={(e) => setStatus(e.target.value)}
-                                                        >
-                                                            <MenuItem value="null">-Ubah Status-</MenuItem>
-                                                            <MenuItem value="accept">Accept</MenuItem>
-                                                            <MenuItem value="reject">Reject</MenuItem>
-                                                        </Select>
-                                                        </FormControl>
-                                                    </TableCell>
-                                                ) : (
-                                                    <TableCell>{row.value}</TableCell>
-                                                )}                                        
-                                            </TableRow>
-                                        ))}
-                                        {status === "reject" ? (
-                                            <TableRow>
-                                                <TableCell>Alasan</TableCell>
-                                                <TableCell>:</TableCell>
-                                                <TableCell>
-                                                     <TextField
-                                                        multiline
-                                                        maxRows={6}
-                                                        minRows={4}
-                                                        variant="outlined"
-                                                        className={classes.formControl}
-                                                    />
-                                                </TableCell>
-                                            </TableRow>
-                                            ) : (
-                                                <TableCell> </TableCell>
-                                            )}
+                                        <PaymentStatusSelector/>
                                     </TableBody>
                                 </Table>
                             </TableContainer>
                         </CardContent>
-                        <CardActions className={classes.cardActions}>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                className={classes.buttonAction}
-                                onClick={handleClickGoBack}
-                                startIcon={<ArrowBackIosRoundedIcon />}
-                                >
-                                Kembali
-                            </Button>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                className={classes.buttonAction}
-                                endIcon={<SaveRoundedIcon />}
-                                // onClick={handleClickNewPayment}
-                                >
-                                Save
-                            </Button>
-                        </CardActions>
                     </Container>
-                    
+                    <CardActions className={classes.cardActions}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            className={classes.buttonAction}
+                            onClick={handleClickGoBack}
+                            startIcon={<ArrowBackIosRoundedIcon />}
+                            >
+                            Kembali
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            className={classes.buttonAction}
+                            endIcon={<SaveRoundedIcon />}
+                            // onClick={handleClickNewPayment}
+                            >
+                            Save
+                        </Button>
+                    </CardActions>
                 </Card>
             </div>
         </ContentContainer>
