1654f57923114c7b065fa0f515d7bca5476825f5
novrianta-sbr <novrianta.sembiring@gmail.com>
2021-09-17 15:04:54 +0700

    feat: Styling PageNotFound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import Logo from "../assets/logoPM.png"
 
 export default function PageNotFound() {
     return (
-        <h1>
-            Page not found
-        </h1>
-    )
+
+        <div className="container"
+            style={{
+                backgroundColor: "#E5E5E5",
+                alignItems: "center",
+            }}>
+
+            <Grid container justify="center" alignItems="center">
+                <img
+                    style={{
+                        height: "90px",
+                    }} src={Logo} />
+
+                <h1
+                    style={{
+                        fontSize: "40px",
+                        marginLeft: "20px"
+                    }}
+                >404 - PAGE NOT FOUND
+
+                    <p
+                        style={{
+                            fontSize: "small",
+                            fontWeight: "lighter",
+                        }}
+                    >The page you are looking for might have been removed had its name changed
+                        <br />or is temporarily unavailable.</p>
+                </h1>
+            </Grid>
+        </div>
+    );
 }
