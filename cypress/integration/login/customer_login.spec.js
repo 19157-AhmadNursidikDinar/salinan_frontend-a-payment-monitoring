ebbe67ca628ac93c84b151195a583d6622de39cf
ardisaurus <ardinisme@gmail.com>
2021-09-27 08:19:07 +0700

    feat : add pesimistic test on login test suites

--- cypress/integration/login/customer_login.spec.js
+++ cypress/integration/login/customer_login.spec.js
@@ -1,10 +1,16 @@
-describe("Customer Login", () => {
+describe("Optimistic Customer Login Test", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
 
+  it("customer can see login form", () => {
+    cy.get("[data-test='txt-username']").should("be.visible");
+    cy.get("[data-test='txt-password']").should("be.visible");
+    cy.get("[data-test='btn-submit']").should("be.visible");
+  });
+
   it("customer can login", () => {
-    cy.get("[data-test='txt-username']").type("myuser");
+    cy.get("[data-test='txt-username']").type("mycustomer3");
     cy.get("[data-test='txt-password']").type("123456");
     cy.get("[data-test='btn-submit']").click();
     cy.findByRole("heading", { name: /daftar payment request/i }).should(
@@ -12,5 +18,36 @@ describe("Customer Login", () => {
     );
     cy.get("[data-test='btn-signout']").click();
     cy.get("[data-test='btn-confirm-signout']").click();
+  });
+});
+
+describe("Pesimistic Customer Login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("show error when both input empty", () => {
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/insert username/i);
+    cy.findByText(/insert password/i);
+  });
+
+  it("show error when username input empty", () => {
+    cy.get("[data-test='txt-password']").type("123456");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/insert username/i);
+  });
+
+  it("show error when password input empty", () => {
+    cy.get("[data-test='txt-username']").type("mycustomer3");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/insert password/i);
+  });
+
+  it("show error when username and password wrong", () => {
+    cy.get("[data-test='txt-username']").type("mycustomer3");
+    cy.get("[data-test='txt-password']").type("1234567");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/password\/username is incorrect/i);
   });
 });
--- cypress/integration/login/officer_login.spec.js
+++ cypress/integration/login/officer_login.spec.js
@@ -1,6 +1,21 @@
-describe("Officer Login", () => {
+describe("Optimistic Officer Login", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/login-officer");
+  });
+
+  it("customer can see login form", () => {
+    cy.get("[data-test='txt-username']").should("be.visible");
+    cy.get("[data-test='txt-password']").should("be.visible");
+    cy.get("[data-test='select-role']").should("be.visible");
+    cy.get("[data-test='select-role']").click();
+    cy.get("[data-test='opt-role-generalSupport']")
+      .should("be.visible")
+      .click();
+    cy.get("[data-test='select-role']").click();
+    cy.get("[data-test='opt-role-accounting']").should("be.visible").click();
+    cy.get("[data-test='select-role']").click();
+    cy.get("[data-test='opt-role-admin']").should("be.visible").click();
+    cy.get("[data-test='btn-submit']").should("be.visible");
   });
 
   it("general support can login", () => {
@@ -44,5 +59,36 @@ describe("Officer Login", () => {
     cy.findByRole("heading", { name: /beranda admin/i }).should("be.visible");
     cy.get("[data-test='btn-signout']").click();
     cy.get("[data-test='btn-confirm-signout']").click();
+  });
+});
+
+describe("Pesimistic Officer Login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/login-officer");
+  });
+
+  it("show error when both input empty", () => {
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/insert username/i);
+    cy.findByText(/insert password/i);
+  });
+
+  it("show error when username input empty", () => {
+    cy.get("[data-test='txt-password']").type("123456");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/insert username/i);
+  });
+
+  it("show error when password input empty", () => {
+    cy.get("[data-test='txt-username']").type("myadmin");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/insert password/i);
+  });
+
+  it("show error when username and password wrong", () => {
+    cy.get("[data-test='txt-username']").type("myadmin");
+    cy.get("[data-test='txt-password']").type("1234567");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/password\/username is incorrect/i);
   });
 });
