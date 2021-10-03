ff0270877608b305647c40e0c3f2c9f6c60af7cf
ardisaurus <ardinisme@gmail.com>
2021-10-03 09:44:55 +0700

    feat : rename service file

---
+++ src/services/user.service.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import authHeader from "../utils/auth-header";
+
+const apiUrl = process.env.REACT_APP_API_BASEURL + "/api/v1/user/create";
+
+class UserService {
+  async CreateNewUser(data) {
+    const { fullname, username, password, role_id, branch_id } = data;
+    console.log(data);
+    try {
+      const response = await axios.post(
+        apiUrl,
+        {
+          fullname,
+          username,
+          password,
+          role_id,
+          branch_id,
+        },
+        {
+          headers: authHeader(),
+        }
+      );
+      return response.data;
+    } catch (error) {
+      return { error };
+    }
+  }
+}
+
+export default new UserService();
