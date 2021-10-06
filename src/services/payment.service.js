458560686f7b53aa30437680529d4b61037305f2
ardisaurus <ardinisme@gmail.com>
2021-10-06 20:55:36 +0700

    feat : auto-formater

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import authHeader from "../utils/auth-header";
 
-export default new PaymentService();
 const apiUrl = process.env.REACT_APP_API_BASEURL + "/api/v1/payment-request";
 
 class PaymentService {
@@ -32,27 +31,36 @@ class PaymentService {
     }
   }
 
-  async insertPayment({ customer_name, request, amount, amount_counted, account_number, account_name, payment_date }) {
+  async insertPayment({
+    customer_name,
+    request,
+    amount,
+    amount_counted,
+    account_number,
+    account_name,
+    payment_date,
+  }) {
     try {
-        const dataPayment =
-        {
-            customer_name: customer_name,
-            request: request,
-            amount: parseInt(amount),
-            amount_counted: amount_counted,
-            account_number: account_number,
-            account_name: account_name,
-            payment_date: payment_date,
-        }
+      const dataPayment = {
+        customer_name: customer_name,
+        request: request,
+        amount: parseInt(amount),
+        amount_counted: amount_counted,
+        account_number: account_number,
+        account_name: account_name,
+        payment_date: payment_date,
+      };
 
-        const response = await axios.post(apiUrl+"/create", dataPayment, { headers: authHeader() })
-        if (response.data) {
-            return response.data
-        }
+      const response = await axios.post(apiUrl + "/create", dataPayment, {
+        headers: authHeader(),
+      });
+      if (response.data) {
+        return response.data;
+      }
     } catch (error) {
-        return { error }
+      return { error };
     }
-}
+  }
 }
 
 export default new PaymentService();
