6dcfbe64196f6762fda448a9d6964866ad975993
ardisaurus <ardinisme@gmail.com>
2021-10-06 21:06:58 +0700

    feat : add pesimistic E2E test

--- cypress/integration/paymentRequest/payment_request.spec.js
+++ cypress/integration/paymentRequest/payment_request.spec.js
@@ -1,4 +1,4 @@
-describe("Customer Login", () => {
+describe("Optimistic case payment request", () => {
   before(() => {
     cy.visit("http://localhost:3000/");
   });
@@ -12,8 +12,17 @@ describe("Customer Login", () => {
     );
   });
 
-  it("customer fill payment request form", () => {
+  it("customer can see payment request form", () => {
     cy.findByRole("button", { name: /payment request/i }).click();
+    cy.get("[data-test='txt-customer_name']").should("be.visible");
+    cy.get("[data-test='select-request']").should("be.visible");
+    cy.get("[data-test='txt-amount']").should("be.visible");
+    cy.get("[data-test='txt-account_name']").should("be.visible");
+    cy.get("[data-test='txt-account_number']").should("be.visible");
+    cy.get("[data-test='btn-submit']").should("be.visible");
+  });
+
+  it("customer fill payment request form", () => {
     cy.get("[data-test='txt-customer_name']").type("mycustomer3");
     cy.get("[data-test='select-request']").click().first().click();
     cy.get("[data-test='txt-amount']").type("2000000");
@@ -21,6 +30,71 @@ describe("Customer Login", () => {
     cy.get("[data-test='txt-account_number']").type("123456789");
     cy.get("[data-test='btn-submit']").click();
     cy.findByText(/payment request berhasil dibuat!/i).should("be.visible");
+  });
+
+  after(() => {
+    cy.get("[data-test='btn-signout']").click();
+    cy.get("[data-test='btn-confirm-signout']").click();
+  });
+});
+
+describe("Pesimistic case payment request", () => {
+  before(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("customer can login", () => {
+    cy.get("[data-test='txt-username']").type("mycustomer3");
+    cy.get("[data-test='txt-password']").type("123456");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByRole("heading", { name: /daftar payment request/i }).should(
+      "be.visible"
+    );
+    cy.findByRole("button", { name: /payment request/i }).click();
+  });
+
+  it("customer need to fill customer name", () => {
+    cy.get("[data-test='txt-amount']").type("2000000");
+    cy.get("[data-test='txt-account_name']").type("recipient1");
+    cy.get("[data-test='txt-account_number']").type("123456789");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/input required!/i).should("be.visible");
+    cy.get("[data-test='txt-amount']").clear();
+    cy.get("[data-test='txt-account_name']").clear();
+    cy.get("[data-test='txt-account_number']").clear();
+  });
+
+  it("customer need to fill payment amount", () => {
+    cy.get("[data-test='txt-customer_name']").type("mycustomer3");
+    cy.get("[data-test='txt-account_name']").type("recipient1");
+    cy.get("[data-test='txt-account_number']").type("123456789");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/input required!/i).should("be.visible");
+    cy.get("[data-test='txt-customer_name']").clear();
+    cy.get("[data-test='txt-account_name']").clear();
+    cy.get("[data-test='txt-account_number']").clear();
+  });
+
+  it("customer need to fill recipient name", () => {
+    cy.get("[data-test='txt-customer_name']").type("mycustomer3");
+    cy.get("[data-test='txt-amount']").type("2000000");
+    cy.get("[data-test='txt-account_number']").type("123456789");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/input required!/i).should("be.visible");
+    cy.get("[data-test='txt-customer_name']").clear();
+    cy.get("[data-test='txt-amount']").clear();
+    cy.get("[data-test='txt-account_number']").clear();
+  });
+
+  it("customer need to fill account number", () => {
+    cy.get("[data-test='txt-customer_name']").type("mycustomer3");
+    cy.get("[data-test='txt-amount']").type("2000000");
+    cy.get("[data-test='txt-account_name']").type("recipient1");
+    cy.get("[data-test='btn-submit']").click();
+    cy.findByText(/input required!/i).should("be.visible");
+    cy.get("[data-test='txt-customer_name']").clear();
+    cy.get("[data-test='txt-amount']").clear();
+    cy.get("[data-test='txt-account_name']").clear();
   });
 
   after(() => {
