cea4e8576d4b4aff5e3b5d052103341ab61759b7
ardisaurus <ardinisme@gmail.com>
2021-10-08 08:55:38 +0700

    feat : fix api url

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import authHeader from "../utils/auth-header";
 
-const apiUrl = process.env.REACT_APP_API_BASEURL + "/api/v1/user/create";
+const apiUrl = process.env.REACT_APP_API_BASEURL + "/api/v1/user";
 
 class UserService {
   async CreateNewUser(data) {
     const { fullname, username, password, role_id, branch_id } = data;
-    console.log(data);
+    // console.log(data);
     try {
       const response = await axios.post(
-        apiUrl,
+        apiUrl + "/create",
         {
           fullname,
           username,
