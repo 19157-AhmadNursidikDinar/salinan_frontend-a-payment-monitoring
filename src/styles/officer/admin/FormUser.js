cf7a4ea4d971e2c6b1503e7f9b760dcff05f5f73
ardisaurus <ardinisme@gmail.com>
2021-10-22 21:40:00 +0700

    feat : add margin for alert

--- src/styles/officer/admin/FormUser.js
+++ src/styles/officer/admin/FormUser.js
@@ -2,28 +2,31 @@ import { makeStyles } from "@material-ui/core/styles";
 import ColorsTheme from "../../../assets/colors";
 
 const useStyles = makeStyles((theme) => ({
-    PaperSize: {
-        paddingTop: theme.spacing(2),
-        paddingBottom: theme.spacing(2),
+  PaperSize: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+  BtnSave: {
+    backgroundColor: ColorsTheme.dodgerBlue,
+    "&:hover": {
+      backgroundColor: ColorsTheme.blueCrayola,
     },
-    BtnSave: {
-        backgroundColor: ColorsTheme.dodgerBlue,
-        "&:hover": {
-            backgroundColor: ColorsTheme.blueCrayola,
-        },
-        marginTop: 30,
-        marginRight: theme.spacing(2),
-        float: 'right',
+    marginTop: 30,
+    marginRight: theme.spacing(2),
+    float: "right",
+  },
+  BtnBack: {
+    backgroundColor: ColorsTheme.dodgerBlue,
+    "&:hover": {
+      backgroundColor: ColorsTheme.blueCrayola,
     },
-    BtnBack: {
-        backgroundColor: ColorsTheme.dodgerBlue,
-        "&:hover": {
-            backgroundColor: ColorsTheme.blueCrayola,
-        },
-        marginTop: 30,
-        marginLeft: theme.spacing(2),
-        float: 'left',
-    },
+    marginTop: 30,
+    marginLeft: theme.spacing(2),
+    float: "left",
+  },
+  alert: {
+    margin: "1em 0",
+  },
 }));
 
 export default useStyles;
